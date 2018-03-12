12382ffa4db019cbab666d5c752b3ebaf58b218e
WallfacerRZD <1048058400@qq.com>
2018-03-12 10:55:53 +0800

    fix bug

--- strategy/FlyBehavior.java
+++ strategy/FlyBehavior.java
@@ -3,12 +3,14 @@ interface FlyBehavior {
 }
 
 class FlyWithWings implements FlyBehavior {
+    @Override
     public void fly() {
         // 实现鸭子飞行
     }
 }
 
 class FlyNoWay implements FlyBehavior {
+    @Override
     public void fly() {
         // 不会飞
     }
--- strategy/QuackBehavior.java
+++ strategy/QuackBehavior.java
@@ -1,22 +1,23 @@
-package QuackBehavior;
-
 interface QuackBehavior {
     public void quack();
 }
 
 class Quack implements QuackBehavior {
+    @Override
     public void quack() {
         // 实现鸭子呱呱叫
     }
 }
 
 class Squeak implements QuackBehavior {
+    @Override
     public void quack() {
         // 橡皮鸭子吱吱叫        
     }
 }
 
 class MuteQuack implements QuackBehavior {
+    @Override
     public void quack() {
         // 不会叫
     }
